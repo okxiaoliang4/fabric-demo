569e6dfe6ac601957d92ca15d220ca8418b8a4ec
Jelf <okxiaoliang4@gmail.com>
2022-11-23 10:07:08 +0800

    feat: copy&paste

--- src/components/FabricCanvas.tsx
+++ src/components/FabricCanvas.tsx
@@ -1,7 +1,8 @@
-import { useWindowSize } from '@vueuse/core';
+import { useMagicKeys, useWindowSize } from '@vueuse/core';
 import { fabric } from 'fabric';
 import { IEvent } from 'fabric/fabric-impl';
-import { defineComponent, provide, onMounted, PropType, ref, shallowRef, onUpdated, watchEffect, useAttrs } from 'vue';
+import { defineComponent, provide, onMounted, PropType, ref, shallowRef, watchEffect } from 'vue';
+import { useFabricCopyPaste } from '../composables/useFabricCopyPaste';
 
 export const FABRIC_CANVAS_SYMBOL = Symbol('fabric-canvas');
 
@@ -19,8 +20,10 @@ export default defineComponent({
     const instance = shallowRef<fabric.Canvas>()
     const canvasEl = ref<HTMLCanvasElement>()
 
+    // 注入fabric canvas实例
     provide(FABRIC_CANVAS_SYMBOL, instance)
 
+    // 初始化实例
     onMounted(() => {
       instance.value = new fabric.Canvas(canvasEl.value!)
       instance.value.on('mouse:wheel', (e) => ctx.emit('mousewheel', e))
@@ -32,8 +35,23 @@ export default defineComponent({
       instance.value?.setWidth(width.value)
     })
 
+    // 更新zoom属性
     watchEffect(() => {
       typeof props.zoom === 'number' && instance.value?.setZoom(props.zoom)
+    })
+
+    // 复制粘贴
+    const { handleCopy,handlePaste } = useFabricCopyPaste(instance)
+    const { command, c, v } = useMagicKeys()
+    watchEffect(() => {
+      if (instance.value) {
+        if (command.value, c.value) {
+          handleCopy()
+        }
+        if (command.value, v.value) {
+          handlePaste()
+        }
+      }
     })
 
     return {
